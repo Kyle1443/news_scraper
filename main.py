57e47355b3debfc2bbc5283101cdb127343901fd
Kyle <kyle1443@gmail.com>
2020-10-25 00:07:29 +0100

    testing

--- main.py
+++ main.py
@@ -6,46 +6,48 @@ import time
 import schedule
 
 
-session = HTMLSession()
-page = 'https://news.google.com/topstories?hl=en-GB&gl=GB&ceid=GB:en'
+def news_email():
+    session = HTMLSession()
+    page = 'https://news.google.com/topstories?hl=en-GB&gl=GB&ceid=GB:en'
 
-r = session.get(page)
-r.html.render(sleep=1, scrolldown=5)
+    r = session.get(page)
+    r.html.render(sleep=1, scrolldown=5)
 
-articles = r.html.find('article')
-link_list = []
+    articles = r.html.find('article')
+    link_list = []
 
-key_words = 'Covid'
+    key_words = 'Covid'
 
-for article in articles:
-    try: 
-        newsitem = article.find('h3', first=True)
-        title = newsitem.text
-        links = newsitem.absolute_links
-        if not key_words in title:continue
-        link_list.append(links)
-        print(str(link_list))
-    except:
-        pass
+    for article in articles:
+        try: 
+            newsitem = article.find('h3', first=True)
+            title = newsitem.text
+            links = newsitem.absolute_links
+            if not key_words in title:continue
+            link_list.append(links)
+            print(str(link_list))
+        except:
+            pass
 
-time.sleep(2)
-
-
-EMAIL_ADRESS = os.environ.get('EMAIL_USER')
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-
-msg = EmailMessage()
-msg['Subject'] = 'Weather alert'
-msg['From'] = EMAIL_ADRESS
-msg['To'] = 'kyle1443@gmail.com'
-msg.set_content(str(link_list))
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
-
-    smtp.send_message(msg)
-
-print('message sent')
+    time.sleep(2)
 
 
-  
+    EMAIL_ADRESS = os.environ.get('EMAIL_USER')
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+    msg = EmailMessage()
+    msg['Subject'] = 'News update'
+    msg['From'] = EMAIL_ADRESS
+    msg['To'] = 'kyle1443@gmail.com'
+    msg.set_content(str(link_list))
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
+
+        smtp.send_message(msg)
+
+    print('message sent')
+
+
+news_email()
+
